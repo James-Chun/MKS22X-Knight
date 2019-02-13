3d28733dc7f8927f54f939addc57bd601027b63d
James Chun <jchun00@stuy.edu>
2019-02-13 10:00:46 -0500

    made toString and checkBoard() to check the status of the board, started working on solve (the exceptions) -- tested all new methods

--- KnightBoard.java
+++ KnightBoard.java
@@ -15,32 +15,59 @@ public class KnightBoard{
 
 
 
-
+  /*
+  blank boards display 0's as underscores
+  you get a blank board if you never called solve or --(Im guessing when you call solve it changes the board so I can leave this as is)
+  when there is no solution
+  */
   public String toString(){
     String visual = "";
     for (int rows=0;rows<board.length;rows++){
-      for (int columns=0;columns<board.length;columns++){
+      for (int columns=0;columns<board[rows].length;columns++){
         if (board[rows][columns]==0){
           visual += "_ ";
         }
       }
-      visuals += "\n";
+      visual += "\n";
     }
     return visual;
   }
-  /*
-  see format for toString below
-  blank boards display 0's as underscores
-  you get a blank board if you never called solve or
-  when there is no solution
-  */
+
+  public String toStringDebug(){    //displays the board in its raw state (with all the numbers)
+    String visual = "";
+    for (int rows=0;rows<board.length;rows++){
+      for (int columns=0;columns<board[rows].length;columns++){
+
+          visual += board[rows][columns]+" ";
+
+      }
+      visual += "\n";
+    }
+    return visual;
+  }
+
+
+  private boolean checkBoard(){   //simply checks the board and sees if it contains only zeroes
+    for (int rows=0;rows<board.length;rows++){
+      for (int columns=0;columns<board[rows].length;columns++){
+        if (board[rows][columns]!=0) return false;
+      }
+    }
+    return true;
+  }
+
 
   /*
   @throws IllegalStateException when the board contains non-zero values.
   @throws IllegalArgumentException when either parameter is negative
    or out of bounds.
    */
-  //public boolean solve(int startingRow, int startingCol)
+  public boolean solve(int startingRow, int startingCol){
+    if (!checkBoard()) {
+      throw new IllegalStateException("ATTEMPTING TO WORK ON NON-EMPTY BOARD");
+    }
+    return true;
+  }
 
   /*
   @throws IllegalStateException when the board contains non-zero values.
@@ -55,8 +82,11 @@ public class KnightBoard{
 
 
   public static void main (String[] args){
-    KnightBoard board = new KnightBoard(args[0], arg[1]);
-    System.out.println(board);
+    KnightBoard board = new KnightBoard(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+    board.board[0][0]=1;
+    //System.out.println(board.toStringDebug());
+    //System.out.println(board);
+    solve()
   }
 
 }
