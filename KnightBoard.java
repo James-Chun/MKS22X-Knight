b3e64dc7ffeda7604be30a1acd5248e830c9245e
James Chun <jchun00@stuy.edu>
2019-02-13 10:15:48 -0500

    added solved() to check is board is solved -- have not tested new code

--- KnightBoard.java
+++ KnightBoard.java
@@ -34,7 +34,7 @@ public class KnightBoard{
   }
 
   public String toStringDebug(){    //displays the board in its raw state (with all the numbers)
-    String visual = "";
+    String visual = "";             //keep in mind that adding a number in board will not be accounted for by the toStrings as it only checks for 0's and adds something if there are
     for (int rows=0;rows<board.length;rows++){
       for (int columns=0;columns<board[rows].length;columns++){
 
@@ -55,6 +55,14 @@ public class KnightBoard{
     }
     return true;
   }
+  private boolean solved(){   //checks if the board is solved (if it contains no 0's then it is solved)
+    for (int rows=0;rows<board.length;rows++){
+      for (int columns=0;columns<board[rows].length;columns++){
+        if (board[rows][columns]==0) return false;
+      }
+    }
+    return true;
+  }
 
 
   /*
@@ -65,7 +73,8 @@ public class KnightBoard{
   public boolean solve(int startingRow, int startingCol){
     if (!checkBoard()) {throw new IllegalStateException("ATTEMPTING TO WORK ON NON-EMPTY BOARD");}
     if (startingRow >= board.length || startingCol >= board[startingRow].length){throw new IllegalArgumentException("INDEX IS OUT OF BOUNDS");}
-    return true;
+
+    return false;
   }
 
   /*
@@ -93,7 +102,7 @@ public class KnightBoard{
     //System.out.println(board);
 
     //board.solve(0,3);
-    
+
   }
 
 }
