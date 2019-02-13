3fedac825a1af4ac96b3e661893f328c6e886a8c
James Chun <jchun00@stuy.edu>
2019-02-13 09:47:43 -0500

    made constructor and started working on toString - have yet to test all new methods

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,16 +1,33 @@
 public class KnightBoard{
   private int[][] board;
 
+
   //@throws IllegalArgumentException when either parameter is negative.
   //Initialize the board to the correct size and make them all 0's
   public KnightBoard(int startingRows,int startingCols){
-    for (int rows=0;rows<board.length)
+    board = new int[startingRows][startingCols];
+    for (int rows=0;rows<board.length;rows++){
+      for (int columns=0;columns<board[rows].length;columns++){
+        board[rows][columns]=0;
+      }
+    }
   }
 
 
 
 
-  public String toString()
+  public String toString(){
+    String visual = "";
+    for (int rows=0;rows<board.length;rows++){
+      for (int columns=0;columns<board.length;columns++){
+        if (board[rows][columns]==0){
+          visual += "_ ";
+        }
+      }
+      visuals += "\n";
+    }
+    return visual;
+  }
   /*
   see format for toString below
   blank boards display 0's as underscores
@@ -23,17 +40,23 @@ public class KnightBoard{
   @throws IllegalArgumentException when either parameter is negative
    or out of bounds.
    */
-  public boolean solve(int startingRow, int startingCol)
+  //public boolean solve(int startingRow, int startingCol)
 
   /*
   @throws IllegalStateException when the board contains non-zero values.
   @throws IllegalArgumentException when either parameter is negative
    or out of bounds.
    */
-  public int countSolutions(int startingRow, int startingCol)
+  //public int countSolutions(int startingRow, int startingCol)
 
   //Suggestion:
-  private boolean solveH(int row ,int col, int level)
+  //private boolean solveH(int row ,int col, int level)
   // level is the # of the knight
+
+
+  public static void main (String[] args){
+    KnightBoard board = new KnightBoard(args[0], arg[1]);
+    System.out.println(board);
+  }
 
 }
