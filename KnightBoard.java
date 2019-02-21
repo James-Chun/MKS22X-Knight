2777ffdf3deb894b717e29727f881ec8ffc1c291
James-Chun <jchun00@stuy.edu>
2019-02-21 16:32:26 -0500

    deleted unnecessary methods and got solve() to compile - need to see if it work properly - also added IllegalARgumentException for constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -6,12 +6,16 @@ public class KnightBoard{
   //@throws IllegalArgumentException when either parameter is negative.
   //Initialize the board to the correct size and make them all 0's
   public KnightBoard(int startingRows,int startingCols){
+    if (startingRows < 0 || startingCols < 0){throw new IllegalArgumentException("BOARD DIMENSIONS CANNOT BE NEGATIVE");}
     board = new int[startingRows][startingCols];
     for (int rows=0;rows<board.length;rows++){
       for (int columns=0;columns<board[rows].length;columns++){
         board[rows][columns]=0;
       }
     }
+    moves = new int[][]{
+      {1,-2},{2,-1},{1,2},{2,1},{-2,-1},{-1,-2},{-1,2},{-2,1}
+    };
   }
 
 
@@ -70,6 +74,7 @@ public class KnightBoard{
     }
     return true;
   }
+  /*
   private boolean solved(){   //checks if the board is solved (if it contains no 0's then it is solved)
     for (int rows=0;rows<board.length;rows++){
       for (int columns=0;columns<board[rows].length;columns++){
@@ -77,7 +82,7 @@ public class KnightBoard{
       }
     }
     return true;
-  }
+  }*/
 
 
   /*
@@ -89,7 +94,7 @@ public class KnightBoard{
     if (!checkBoard()) {throw new IllegalStateException("ATTEMPTING TO WORK ON NON-EMPTY BOARD");}
     if (startingRow >= board.length || startingCol >= board[startingRow].length){throw new IllegalArgumentException("INDEX IS OUT OF BOUNDS");}
 
-    return solveH(startingRow,startingCol,0);
+    return solveH(startingRow,startingCol,1);
   }
 
   /*
@@ -107,75 +112,19 @@ public class KnightBoard{
   //Suggestion:
 
   private boolean solveH(int row ,int col, int level){    //need to call possibleMOves somewhere here
-    if (solved())return true;
+    if (level==row*col)return true;
+    if (row<0 || col<0 || row>board.length || col>board[0].length)return false;
 
     for (int m=0;m<moves.length;m++){       //simply a temp psuedo code
-      if (moves[m]== solveH(startingRow+moves[m][0],startingCol+moves[m][1],level+1) ){   //remember, some values in moves will be null so check for that
-        return sol
-      }
+      board[row][col]=level;
 
-      removeQueen(rows,column);       //Do the same as queens except instead of going through all the rows, go through all possible moves
+      if ( solveH(row+moves[m][0],col+moves[m][1],level+1) && board[row+moves[m][0]][col+moves[m][1]]==0 ){
+        return true;
+      }
+      board[row][col]=0;
+        //Do the same as queens except instead of going through all the rows, go through all possible moves
     }
     return false;
-  }
-
-
-
-  private void possibleMoves(int currentRow, int currentCol){   //takes current coords of the knight and finds all possible moves.
-    moves = new int[8][2];    //clearing the moves array [simply makes a list the size of all possible moves]
-    int temp=0;   //keeping track of location in moves
-    if (currentRow-2>=0){   //each array in moves is possible routes, each number in each subarray are specific directions (first is moving up or down[- || +] and second is moving left or right[- || +])
-      if (currentCol-1>=0){
-        moves[temp][0]=-2;        //dont do this just yet
-        moves[temp][1]=-1;        //the solve methods will have checks to see if given directions work
-        temp++;
-      }
-      if (currentCol+1<=board[0].length){
-        moves[temp][0]=-2;
-        moves[temp][1]=1;                       //this works just make it cleaner later I guess
-        temp++;
-      }
-    }
-
-    if (currentRow+2<=board[0].length){
-      if (currentCol-1>=0){
-        moves[temp][0]=2;
-        moves[temp][1]=-1;
-        temp++;
-      }
-      if (currentCol+1<=board[0].length){
-        moves[temp][0]=2;
-        moves[temp][1]=1;
-        temp++;
-      }
-    }
-
-    if (currentRow-1>=0){
-      if (currentCol-2>=0){
-        moves[temp][0]=-1;
-        moves[temp][1]=-2;
-        temp++;
-      }
-      if (currentCol+2<=board[0].length){
-        moves[temp][0]=-1;
-        moves[temp][1]=2;
-        temp++;
-      }
-    }
-
-    if (currentRow+1<=board[0].length){
-      if (currentCol-2>=0){
-        moves[temp][0]=1;
-        moves[temp][1]=-2;
-        temp++;
-      }
-      if (currentCol+2<=board[0].length){
-        moves[temp][0]=1;
-        moves[temp][1]=2;
-        temp++;
-      }
-    }
-
   }
 
 
@@ -190,9 +139,10 @@ public class KnightBoard{
 
     System.out.println(board.toStringDebug());
     //System.out.println(board);
-
-
-    System.out.println(board.moves());
+    //System.out.println(board.moves());
+    System.out.println(board);
+    board.solve(0,0);
+    System.out.println(board);
   }
 
 }
