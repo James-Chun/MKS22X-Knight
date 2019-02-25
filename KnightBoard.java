33b1a363bb662c3687ac73d25e00902fe72fe502
James-Chun <jchun00@stuy.edu>
2019-02-25 14:09:03 -0500

    tested using Mr. K's tests

--- KnightBoard.java
+++ KnightBoard.java
@@ -164,24 +164,47 @@ public class KnightBoard{
     }
   }
 
-
-
-
   // level is the # of the knight
 
 
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//TESTING
+  //testcase must be a valid index of your input/output array
+  public static void runTest(int i){
+
+    KnightBoard b;
+    int[]m =   {4,5,5,5,5};
+    int[]n =   {4,5,4,5,5};
+    int[]startx = {0,0,0,1,2};
+    int[]starty = {0,0,0,1,2};
+    int[]answers = {0,304,32,56,64};
+    if(i >= 0 ){
+      try{
+        int correct = answers[i];
+        b = new KnightBoard(m[i%m.length],n[i%m.length]);
+
+        int ans  = b.countSolutions(startx[i],starty[i]);
+
+        if(correct==ans){
+          System.out.println("PASS board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans);
+        }else{
+          System.out.println("FAIL board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans+" vs "+correct);
+        }
+      }catch(Exception e){
+        System.out.println("FAIL Exception case: "+i);
+
+      }
+    }
+  }
+
   public static void main (String[] args){
-    KnightBoard board = new KnightBoard(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
-
-
-    //System.out.println(board.toStringDebug());
-    //System.out.println(board);
-    //System.out.println(board.moves());
-  //  System.out.println(board);
-      //board.solve(0,0);
-    System.out.println(board.countSolutions(Integer.parseInt(args[2]),Integer.parseInt(args[3])));
-    System.out.println(board);
-    //System.out.println(board.toStringDebug());
+    /*
+    runTest(0);
+    runTest(1);
+    runTest(2);
+    runTest(3);
+    runTest(4);
+    */
   }
 
 }
